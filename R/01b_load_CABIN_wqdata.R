3c92bfd4d387fee00c02f0c7bb90b37fe45f01ba
Heather Thompson <heather.c.thompson@gov.bc.ca>
2019-02-11 16:45:20 -0800

    edited to recent date to run script

--- R/01b_load_CABIN_wqdata.R
+++ R/01b_load_CABIN_wqdata.R
@@ -11,16 +11,17 @@
 # See the License for the specific language governing permissions and limitations under the License. 
 
 ############################################################################### 
-# Module 1b. LOAD CABIN WATER QUALITY DATA FROM EMS
+## Module 1b. LOAD CABIN WATER QUALITY DATA FROM EMS
 ############################################################################### 
 
 ## LOAD TWO YEAR EMS WATER QUALITY DATASET from BC Data Catalogue using bcgov/rems package
 
 twoyear <- get_ems_data(which = "2yr", ask = TRUE)
 
+## These are CABIN reference sites in Northeast B.C.
 filtered_twoyear_env_cabin <- filter_ems_data(twoyear, 
                                     emsid = c("E273193","E273194","E273195","E273196","E273197","E273198","E273199","E273200","E298950","E306409","E306408","E306407","E306400","E306399","E306398","E306397","E273188","E273189","E273190","E273191","E273192","E306405","E306401","E306403","E295021","E309217","E309218","E309547","E243581","E303831","E303845","E305437","E305433","E303830","E303851","E305434","E306404","E309216","E309215","E309189"), 
-                                    to_date = "2017/10/31") 
+                                    to_date = "2019/02/11") 
 
                   
 tidy_data_env_cabin <- tidy_ems_data(filtered_twoyear_env_cabin, mdl_action = "mdl")
@@ -31,6 +32,7 @@ all_data_clean_env_cabin <- clean_wqdata(all_data_env_cabin, by = "EMS_ID", dele
 
 cabin_env_sites <- distinct(all_data_clean_env_cabin, EMS_ID)
 
+## Create summary table of Min and Max of each parameter.
 cabin_summary <- group_by(all_data_clean_env_cabin, Variable, Units) %>% 
   summarise(Min=min(Value), Max=max(Value))
 
